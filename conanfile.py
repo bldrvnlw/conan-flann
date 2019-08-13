fa2695974c220dad34fd9a2bbfd8753bccf965fc
Baldur van Lew <baldurvanlew@gmail.com>
2019-08-13 16:09:54 +0200

    First checking flann 1.8.5

---
+++ conanfile.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+
+from conans import ConanFile, CMake, tools
+import os
+
+
+class FlannConan(ConanFile):
+    name = "libname"
+    version = "1.8.5"
+    description = "3rd party library for Fast Library for Approximate Nearest Neighbors by Marius Muja and David Lowe"
+    # topics can get used for searches, GitHub topics, Bintray tags etc. Add here keywords about the library
+    topics = ("nearest neighbor", "high dimensions", "approximated")
+    url = "https://github.com/bldrvnlw/conan-flann"
+    homepage = "https://github.com/mariusmuja/flann"
+    author = "B. van Lew <b.van_lew@lumc.nl>"
+    license = "BSD-3-Clause"  # Indicates license type of the packaged library; please use SPDX Identifiers https://spdx.org/licenses/
+    exports = ["LICENSE.md"]      # Packages the license for the conanfile.py
+    # Remove following lines if the target lib does not use cmake.
+    exports_sources = ["CMakeLists.txt"]
+    generators = "cmake"
+
+    # Options may need to change depending on the packaged library.
+    settings = "os", "arch", "compiler", "build_type"
+    # Include Position Independent Code option (removed for Windows)
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+
+    # Custom attributes for Bincrafters recipe conventions
+    _source_subfolder = "source_subfolder"
+    _build_subfolder = "build_subfolder"
+
+    #requires = (
+    #    "OpenSSL/1.0.2s@conan/stable",
+    #    "zlib/1.2.11@conan/stable"
+    #)
+
+    def config_options(self):
+        if self.settings.os == 'Windows':
+            del self.options.fPIC
+
+    def source(self):
+        self.run("git clone https://github.com/mariusmuja/flann.git")
+        os.chdir("./flann")
+        self.run("git checkout tags/{0}".format(self.version))
+        # This small hack might be useful to guarantee proper /MT /MD linkage
+        # in MSVC if the packaged project doesn't have variables to set it
+        # properly
+        conanproj = ("project(flann)\n" 
+                "include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)\n"
+                "conan_basic_setup()"
+        )     
+        os.chdir("..")
+        tools.replace_in_file("flann/CMakeLists.txt", "project(flann)", conanproj)
+
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.definitions["BUILD_TESTS"] = False  # example
+        cmake.configure(build_folder=self._build_subfolder)
+        return cmake
+
+    def build(self):
+        # self.run("cmake --build . --target help")
+        cmake = CMake(self)
+        # <bvl> These don't work out of the box on Windows and are not needed
+        # for my environment. If someone can get them working that would be great!
+        cmake.definitions["BUILD_PYTHON_BINDINGS"] = "OFF"
+        cmake.definitions["BUILD_MATLAB_BINDINGS"] = "OFF" 
+        cmake.definitions["BUILD_TESTS"] = "OFF"
+        cmake.definitions["BUILD_EXAMPLES"] = "OFF"        
+        # work around failure to produce .lib file for flann_cpp        
+        if self.settings.os == "Windows" and self.options.shared:
+            cmake.definitions["CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS"] = True            
+        cmake.configure(source_folder="flann")
+        cmake.verbose = True
+        cmake.build()
+
+    def package(self):
+        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
+        cmake = self._configure_cmake()
+        cmake.install()
+        # If the CMakeLists.txt has a proper install method, the steps below may be redundant
+        # If so, you can just remove the lines below
+        self.copy("*.h", src="flann/src/cpp", dst="include", keep_path=True)
+        self.copy("*.hpp", src="flann/src/cpp", dst="include", keep_path=True)  
+        self.copy("*flann.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
